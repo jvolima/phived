28f88c2b3a27a194662db5adb9a11f493c1394ce
João Vitor Lima <jvolima2004@gmail.com>
2023-03-21 11:12:59 -0300

    feat: add InstallPwa Button

---
+++ src/components/InstallPwa/InstallPwa.tsx
@@ -0,0 +1,48 @@
+import type { MouseEvent } from "react";
+import { useEffect, useState } from "react";
+import "src/components/InstallPwa/styles.css";
+
+interface BeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+  outcome: "accepted" | "dismissed";
+  platform: string;
+  }>;
+  prompt(): Promise<void>;
+  }
+
+export function InstallPwa() {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState<BeforeInstallPromptEvent | null>(null);
+
+  useEffect(() => {
+    const handler = (e: Event) => {
+      e.preventDefault();
+      setSupportsPWA(true);
+      setPromptInstall(e as BeforeInstallPromptEvent);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+
+  const onClick = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+  if (!supportsPWA) {
+    return null;
+  }
+  return (
+    <button
+      className="install"
+      aria-label="Install PWA"
+      onClick={onClick}
+    >
+      Install PWA
+    </button>
+  );
+}
---
+++ src/components/InstallPwa/index.ts
@@ -0,0 +1,1 @@
+export { InstallPwa } from "src/components/InstallPwa/InstallPwa"; 
